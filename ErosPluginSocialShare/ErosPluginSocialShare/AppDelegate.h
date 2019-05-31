9c87b64fb10058cac7a51e02d39b994b87484afb
luke <luke.liu@sjfood.com>
2019-05-31 02:42:40 -0700

    Update： 新创建项目

---
+++ ErosPluginSocialShare/ErosPluginSocialShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ErosPluginSocialShare
+//
+//  Created by Luke on 5/31/19.
+//  Copyright © 2019 LUKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ErosPluginSocialShare/ErosPluginSocialShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ErosPluginSocialShare
+//
+//  Created by Luke on 5/31/19.
+//  Copyright © 2019 LUKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
