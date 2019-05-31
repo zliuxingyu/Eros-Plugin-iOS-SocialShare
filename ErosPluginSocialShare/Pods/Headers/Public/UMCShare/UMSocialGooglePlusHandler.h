bae0dbb525898d460f0a186742f2eacc658b0e75
luke <luke.liu@sjfood.com>
2019-05-31 03:27:05 -0700

    Update: 导入WeexSDK，友盟社交等第三方库

---
+++ ErosPluginSocialShare/Pods/Headers/Public/UMCShare/UMSocialGooglePlusHandler.h
@@ -0,0 +1,1 @@
+../../../UMCShare/UMShare/SocialLibraries/GooglePlus/UMSocialGooglePlusHandler.h
---
+++ ErosPluginSocialShare/Pods/Target Support Files/Pods-ErosPluginSocialShareUITests/Pods-ErosPluginSocialShareUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ErosPluginSocialShareUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ErosPluginSocialShareUITests
+@end
